f2cfd4c97802f223639ec7d249bf1f8c311f207f
Peter Pitterling <peter.pitterling@sap.com>
2019-11-10 08:53:27 +0100

    Unittests - shebangs,prevent loading original libraries (#657)
    * change bash shebangs to modern path
    
    * prevent loading of original libraries whcih are not required for the current test

--- scripts/tests/CompareToobigNumbers_test.sh
+++ scripts/tests/CompareToobigNumbers_test.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 set -u      # treat unset variables as an error
 
 PROGRAM_DIR="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
@@ -76,9 +76,27 @@ testCompareTooBigNumbersShouldFail() {
     assertNotEquals 'test[3]: testing the tester failed' '2' "${_rc}"
 }
 
-# oneTimeSetUp () {
+oneTimeSetUp () {
 
-# }
+    # prevent loading of original libraries
+    # shellcheck disable=SC2034
+    LIB_LINUX_RELEASE='dont load'
+    # shellcheck disable=SC2034
+    LIB_PLATF_x86_64_RELEASE='dont load'
+    # shellcheck disable=SC2034
+    LIB_PLATF_POWER_RELEASE='dont load'
+
+    declare -i LIB_PLATF_RAM_MIB_PHYS=1024
+    declare -i LIB_PLATF_RAM_MiB_AVAILABLE=1024
+
+    #shellcheck source=./saphana-logger-stubs
+    source "${PROGRAM_DIR}/./saphana-logger-stubs"
+
+    #shellcheck source=../bin/saphana-helper-funcs
+    source "${PROGRAM_DIR}/../bin/saphana-helper-funcs"
+
+ }
+
 # oneTimeTearDown
 # setUp
 # tearDown
@@ -86,7 +104,6 @@ testCompareTooBigNumbersShouldFail() {
 #Import Libraries
 # - order is important - sourcing shunit triggers testing
 # - that's also the reason, why it could not be done during oneTimeSetup
-#shellcheck source=../bin/saphana-helper-funcs
-source "${PROGRAM_DIR}/../bin/saphana-helper-funcs"
+
 #shellcheck source=./shunit2
 source "${PROGRAM_DIR}/shunit2"
--- scripts/tests/NormalizeKernel_test.sh
+++ scripts/tests/NormalizeKernel_test.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 set -u      # treat unset variables as an error
 
 PROGRAM_DIR="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
@@ -42,9 +42,27 @@ testNormalizeKernelShouldFail() {
     assertNotEquals 'test[1]: testing the tester failed' '3.0.101-0.47.71.1' "${kernelversion}"
 }
 
-# oneTimeSetUp () {
+oneTimeSetUp () {
 
-# }
+    # prevent loading of original libraries
+    # shellcheck disable=SC2034
+    LIB_LINUX_RELEASE='dont load'
+    # shellcheck disable=SC2034
+    LIB_PLATF_x86_64_RELEASE='dont load'
+    # shellcheck disable=SC2034
+    LIB_PLATF_POWER_RELEASE='dont load'
+
+    declare -i LIB_PLATF_RAM_MIB_PHYS=1024
+    declare -i LIB_PLATF_RAM_MiB_AVAILABLE=1024
+
+    #shellcheck source=./saphana-logger-stubs
+    source "${PROGRAM_DIR}/./saphana-logger-stubs"
+
+    #shellcheck source=../bin/saphana-helper-funcs
+    source "${PROGRAM_DIR}/../bin/saphana-helper-funcs"
+
+ }
+
 # oneTimeTearDown
 # setUp
 # tearDown
@@ -52,7 +70,6 @@ testNormalizeKernelShouldFail() {
 #Import Libraries
 # - order is important - sourcing shunit triggers testing
 # - that's also the reason, why it could not be done during oneTimeSetup
-#shellcheck source=../bin/saphana-helper-funcs
-source "${PROGRAM_DIR}/../bin/saphana-helper-funcs"
+
 #shellcheck source=./shunit2
 source "${PROGRAM_DIR}/shunit2"
