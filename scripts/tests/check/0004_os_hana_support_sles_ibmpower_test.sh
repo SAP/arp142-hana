0f9e895a494a4b26f375a2f9674eaf74f5c6caf8
Peter Pitterling <peter.pitterling@sap.com>
2023-10-06 16:31:18 +0200

    SLES15.5 fix for IBM Power (#1308)

--- scripts/tests/check/0004_os_hana_support_sles_ibmpower_test.sh
+++ scripts/tests/check/0004_os_hana_support_sles_ibmpower_test.sh
@@ -114,7 +114,7 @@ test_power10_littleendian_sles_supported() {
     #arrange
     LIB_PLATF_ARCHITECTURE='ppc64le'
     LIB_PLATF_POWER_PLATFORM_BASE='POWER10'
-    OS_VERSION='15.3'
+    OS_VERSION='15.5'
 
     #act
     check_0004_os_hana_support_sles_ibmpower
