8ad0bea80612104d62c62797bd6446385b0d613f
Peter Pitterling <peter.pitterling@sap.com>
2024-02-10 11:56:43 +0100

    nfs_mount_rwsize - new unittest for amazon, harmonize checks, refactor rx variable, simplify unittest grep fake (#1377)

--- scripts/tests/check/5522_nfs_mount_rwsize_amazon_test.sh
+++ scripts/tests/check/5522_nfs_mount_rwsize_amazon_test.sh
@@ -5,7 +5,7 @@ PROGRAM_DIR="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
 readonly PROGRAM_DIR
 
 #mock PREREQUISITE functions
-LIB_FUNC_IS_CLOUD_MICROSOFT() { return 0 ; }
+LIB_FUNC_IS_CLOUD_AMAZON() { return 0 ; }
 LIB_FUNC_STRINGCONTAIN() { [[ -z "${1##*$2*}" ]] && [[ -z "$2" || -n "$1" ]]; }
 
 grep() {
@@ -29,7 +29,7 @@ test_nfs_not_mounted() {
     nfs_mounts+=('//cpsapnfsstoracc01/cgadmin /mnt/cpsapnfsstoracc01 cifs nofail,vers=3.0,credentials=/etc/smbcredentials/cpsapnfsstoracc01.cred,dir_mode=0777')
 
     #act
-    check_5521_nfs_mount_rwsize_azure
+    check_5522_nfs_mount_rwsize_amazon
 
     #assert
     assertEquals "CheckSkipped? RC" '3' "$?"
@@ -44,7 +44,7 @@ test_nfs_ok() {
     nfs_mounts+=('0.0.0.0:/vol /SID/mnt00001 nfs rw,noatime,vers=3,rsize=262144,wsize=262144,hard,proto=tcp')
 
     #act
-    check_5521_nfs_mount_rwsize_azure
+    check_5522_nfs_mount_rwsize_amazon
 
     #assert
     assertEquals "CheckOk? RC" '0' "$?"
@@ -57,7 +57,7 @@ test_nfs_rsize_wrong() {
     nfs_mounts+=('0.0.0.0:/vol /SID/mnt00001 nfs4 rw,noatime,vers=4.1,rsize=1048576,wsize=262144,hard,proto=tcp')
 
     #act
-    check_5521_nfs_mount_rwsize_azure
+    check_5522_nfs_mount_rwsize_amazon
 
     #assert
     assertEquals "CheckWarn? RC" '1' "$?"
@@ -70,7 +70,7 @@ test_nfs_wsize_wrong() {
     nfs_mounts+=('0.0.0.0:/vol /SID/mnt00001 nfs4 rw,noatime,vers=4.1,rsize=262144,wsize=1048576,hard,proto=tcp')
 
     #act
-    check_5521_nfs_mount_rwsize_azure
+    check_5522_nfs_mount_rwsize_amazon
 
     #assert
     assertEquals "CheckWarn? RC" '1' "$?"
@@ -85,7 +85,7 @@ test_nfs_wrong_all() {
     nfs_mounts+=('0.0.0.0:/vol /SID/mnt00001 nfs rw,noatime,vers=3,rsize=1048576,wsize=1048576,hard,proto=tcp')
 
     #act
-    check_5521_nfs_mount_rwsize_azure
+    check_5522_nfs_mount_rwsize_amazon
 
     #assert
     assertEquals "CheckWarn? RC" '1' "$?"
@@ -96,8 +96,8 @@ oneTimeSetUp() {
     #shellcheck source=../saphana-logger-stubs
     source "${PROGRAM_DIR}/../saphana-logger-stubs"
 
-    #shellcheck source=../../lib/check/5521_nfs_mount_rwsize_azure.check
-    source "${PROGRAM_DIR}/../../lib/check/5521_nfs_mount_rwsize_azure.check"
+    #shellcheck source=../../lib/check/5522_nfs_mount_rwsize_amazon.check
+    source "${PROGRAM_DIR}/../../lib/check/5522_nfs_mount_rwsize_amazon.check"
 
 }
 
